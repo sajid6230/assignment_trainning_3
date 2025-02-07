81f25674afeb5c7643509476103bac55f154d48b
sajid6230 <sajid6230@gmail.com>
2025-02-07 11:14:08 -0600

    6th Problem

---
+++ function_exercise/6.py
@@ -0,0 +1,13 @@
+'''
+6. Write a function to print the letters of the given name.
+'''
+
+def print_letters(name):
+    for i in name:
+        print(i)
+
+
+
+#calling the function
+name = input('Enter a name: ')
+print_letters(name)
