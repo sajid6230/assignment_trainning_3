da2c575b340988629eb051d1e9798167af6968d9
sajid6230 <sajid6230@gmail.com>
2025-02-06 19:14:32 -0600

    1st problem

---
+++ function_exercise/1.py
@@ -0,0 +1,24 @@
+'''
+1. Write a function to accept your name and print hello <name> using the below structure:
+
+a. Without parameters
+
+b. With parameters
+'''
+
+#function without parameter
+def my_name():
+    name = input("Enter your name: ")
+    print(f"hello {name}")
+
+
+#function with parameter
+def my_name1(name):
+    print(f'hello {name}')
+
+
+
+
+
+my_name()
+my_name1('Sajid')
